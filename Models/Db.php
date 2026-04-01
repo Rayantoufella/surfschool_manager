7a32e06520ffb3b651b65d2d1d7573cc3dc36dc3
Rayan Toufella <rayantoufella3@gmail.com>
2026-04-01 22:41:35 +0100

    la connexion a Data base

--- Models/Db.php
+++ Models/Db.php
@@ -1,1 +1,48 @@
 <?php
+
+class Db {
+
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $db = "school";
+    private $pdo ;
+
+
+
+    public function __construct($host, $user, $pass, $db) {
+        $this->host = $host ;
+        $this->user = $user ;
+        $this->pass =$pass ;
+        $this->db =$db ;
+
+    }
+
+
+
+
+    public function connect(){
+        try {
+            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db ;
+
+            $this->pdo = new PDO($dsn ,$this->user , $this->pass) ;
+
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $this->pdo ;
+
+
+        }catch(PDOException $e){
+            die('Erreur : ' . $e->getMessage());
+        }
+    }
+
+    public function getPDO(){
+        if($this->pdo == null){
+            $this->connect();
+        }
+        return $this->pdo ;
+    }
+
+}
+
--- config/database.php
+++ config/database.php
@@ -1,1 +1,4 @@
 <?php
+require_once __DIR__ . '/../Models/Db.php';
+$db = new Db('localhost', 'root', '', 'surfschool', null);
+?>
